5b6e793045dcac08757e86d2568bf339899c8839
Maria <mariain@llee-mac.local>
2017-06-20 12:03:19 -0700

    Initial Development

--- InstagramClone/FeedTableViewController.swift
+++ InstagramClone/FeedTableViewController.swift
@@ -75,21 +75,12 @@ class FeedTableViewController: UITableViewController {
             }
         })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return usernames.count
     }
 
@@ -98,67 +89,12 @@ class FeedTableViewController: UITableViewController {
         let myCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! cell
         
         imageFiles[indexPath.row].getDataInBackground { (data, error) -> Void in
-            
             if let downloadedImage = UIImage(data: data!) {
-                
                 myCell.postedImage.image = downloadedImage
-                
             }
-            
         }
-        
-        
-        
         myCell.username.text = usernames[indexPath.row]
-        
         myCell.message.text = messages[indexPath.row]
-        
         return myCell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
